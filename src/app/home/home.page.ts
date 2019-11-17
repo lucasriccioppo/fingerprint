8bb8991494734a48be40bd5005c2cb5af9f7fdcc
lucas.riccioppo <lucas.riccioppo@valecard.com.br>
2019-11-17 13:02:58 -0200

    add native component.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private faio: FingerprintAIO) {}
 
+  teste = () => {
+    console.log("Starting...")
+    this.faio.show({
+      clientId: 'Fingerprint-Demo', //Android: Used for encryption. iOS: used for dialogue if no `localizedReason` is given.
+      clientSecret: 'fingerprinsecret', //Necessary for Android encrpytion of keys. Use random secret key.
+  })
+  .then((result: any) => console.log(result))
+  .catch((error: any) => console.log(error));
+  }
 }
